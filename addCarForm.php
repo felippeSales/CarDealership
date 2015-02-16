42ae5b5d9314ecab782eaac53e298483a0ab2601
Felipe Sales <felipesales@MacBook-Pro-de-Felipe.local>
2015-02-16 03:49:11 -0800

    Add cars implemented

--- addCarForm.php
+++ addCarForm.php
@@ -3,41 +3,41 @@ include 'header.php';
 ?>
 
 <div class="container">
-    <form class="form-horizontal" role="form" id="new-car-form">
+    <form class="form-horizontal" role="form" id="new-car-form" action="addCarHandler.php" method="POST">
         <div class="form-group">
             <label for="new-car-year" class="col-sm-3 control-label">Year created:</label>
             <div class="col-sm-9">
-                <input class="form-control" id="new-car-year" type="number" placeholder="Year created">
+                <input class="form-control" name="new-car-year" type="number" placeholder="Year created">
             </div>
         </div>
         <div class="form-group">
             <label for="new-car-name" class="col-sm-3 control-label">Model Name:</label>
             <div class="col-sm-9">
-                <input class="form-control" id="new-car-name" type="text" placeholder="Model Name">
+                <input class="form-control" name="new-car-name" type="text" placeholder="Model Name">
             </div>
         </div>
         <div class="form-group">
             <label for="new-car-mileage" class="col-sm-3 control-label">Mileage:</label>
             <div class="col-sm-9">
-                <input class="form-control" id="new-car-mileage" type="number" placeholder="Mileage">
+                <input class="form-control" name="new-car-mileage" type="number" placeholder="Mileage">
             </div>
         </div>
         <div class="form-group">
             <label for="new-car-max-speed" class="col-sm-3 control-label">Maximal Speed:</label>
             <div class="col-sm-9">
-                <input class="form-control" id="new-car-max-speed" type="number" placeholder="Max speed">
+                <input class="form-control" name="new-car-max-speed" type="number" placeholder="Max speed">
             </div>
         </div>
         <div class="form-group">
             <label for="new-car-desc" class="col-sm-3 control-label">Description:</label>
             <div class="col-sm-9">
-                <textarea class="form-control" id="edit-prop-desc" placeholder="Description" rows="4"></textarea>
+                <textarea class="form-control" name="new-car-desc" placeholder="Description" rows="4"></textarea>
             </div>
         </div>
         <div class="form-group">
             <label for="new-car-sold" class="col-sm-3 control-label">Sold:</label>
             <div class="col-sm-9">
-                <input class="form-control" id="new-car-sold" type="number" placeholder="Sold in">
+                <input class="form-control" name="new-car-sold" type="number" placeholder="Sold in">
                 
             </div>
             <div class="col-sm-9">
--- addCarHandler.php
+++ addCarHandler.php
@@ -1,8 +1,18 @@
 <?php
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+require_once 'database.php';
+
+$database = RegularDB::getInstance();
+
+$year = $_POST['new-car-year'];
+$name = $_POST['new-car-name'];
+$mileage = $_POST['new-car-mileage'];
+$speed = $_POST['new-car-max-speed'];
+$desc = $_POST['new-car-desc'];
+$sold = $_POST['new-car-sold'];
+
+$columnsValues = array("model_name" => $name, "year_built" => $year, "mileage" => $mileage, "max_speed" => $speed
+    , "description" => $desc, "sold_date" => $sold);
+
+$database->insertIntoTable("car", $columnsValues);
 
